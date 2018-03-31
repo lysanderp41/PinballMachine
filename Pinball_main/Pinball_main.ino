4853a375a8023c0cc4e52b1d125043a53935c83c
Riley Wong <rileywong96@gmail.com>
2018-03-31 18:50:16 -0400

    initial commit

---
+++ Pinball_main/Pinball_main.ino
@@ -0,0 +1,13 @@
+//Pin hole tests
+const int hole1 = 0;
+const int hole2 = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
